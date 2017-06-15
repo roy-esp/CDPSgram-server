9ef16bb6fc3db29c83a2f6d133d6c6beb6b7b08d
roy-esp <adrianroy1@hotmail.com>
2017-06-15 17:27:23 +0200

    Update photo.js

--- models/photo.js
+++ models/photo.js
@@ -12,22 +12,6 @@ exports.photos = {
 	1: {
 		name: 'Wood',
 		url: photos_url + '/photos/photo1.jpg'
-	},
-	2: {
-		name: 'Desktop',
-		url: photos_url + '/photos/photo2.jpg'
-	},
-	3: {
-		name: 'Man',
-		url: photos_url + '/photos/photo3.jpg'
-	},
-	4: {
-		name: 'Woman',
-		url: photos_url + '/photos/photo4.jpg'
-	},
-	5: {
-		name: 'People',
-		url: photos_url + '/photos/photo5.jpg'
 	}
 }; 
 
