d00402dfbca6eaa425d8b39f46641b7e3a2e0d02
roy-esp <adrianroy1@hotmail.com>
2017-06-15 15:58:57 +0200

    No imagenes inicio+ lb en vez de server

--- models/photo.js
+++ models/photo.js
@@ -9,27 +9,4 @@ photo_id: {
 
 */
 
-var photos_url = process.env.PHOTOS_URL || "http://10.1.3.12:8000"
-
-exports.photos = {
-	1: {
-		name: 'Wood',
-		url: photos_url + '/photos/photo1.jpg'
-	},
-	2: {
-		name: 'Desktop',
-		url: photos_url + '/photos/photo2.jpg'
-	},
-	3: {
-		name: 'Man',
-		url: photos_url + '/photos/photo3.jpg'
-	},
-	4: {
-		name: 'Woman',
-		url: photos_url + '/photos/photo4.jpg'
-	},
-	5: {
-		name: 'People',
-		url: photos_url + '/photos/photo5.jpg'
-	}
-};
+var photos_url = process.env.PHOTOS_URL || "http://10.1.3.1:8000"
